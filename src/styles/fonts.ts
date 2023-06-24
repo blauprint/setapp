dc923c9ed83587b766917819f77dd47e3358bcc5
Arun Pariyar <arun.pariyar@gmail.com>
2023-06-24 11:32:10 +0200

    build(navigation): added navigation bar
    I am also made changes to the globals.css and please note that that navigation used in the index.tsx
    so that it can be made available on everypage

---
+++ src/styles/fonts.ts
@@ -0,0 +1,6 @@
+import { Montserrat , Source_Code_Pro } from "next/font/google";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
+const sourceCodePro = Source_Code_Pro({subsets: ["latin"] })
+
+export { montserrat, sourceCodePro };
