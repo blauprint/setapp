94049980532fafa083a6a034adcb927f9318fa1a
Germán Piccioni <german.n.piccioni@gmail.com>
2023-06-24 11:44:36 +0200

    Delete fonts.ts
    PR fix

--- src/styles/fonts.ts
+++
@@ -1,6 +0,0 @@
-import { Montserrat , Source_Code_Pro } from "next/font/google";
-
-const montserrat = Montserrat({ subsets: ["latin"] });
-const sourceCodePro = Source_Code_Pro({subsets: ["latin"] })
-
-export { montserrat, sourceCodePro };
