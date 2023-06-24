c3be1165c0ed5b5231e28e479a8818a2abdb3505
Arun Pariyar <arun.pariyar@gmail.com>
2023-06-24 17:05:10 +0200

    feat(projectcard component): i have incorporated a more isomorphic design for the project cards
    BREAKING CHANGE: It is specific to the mock data and project card component

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,11 +1,37 @@
 import { Project } from "@/types/Project";
-import styles from "@/styles/ProjectCard.module.css"
+import styles from "@/styles/ProjectCard.module.css";
 
 export default function ProjectCard({ project }: { project: Project }) {
-
   return (
     <div className={styles.projectCard}>
-      <h3>{project.title}</h3>
-      <p>{project.create_date.toDateString()}</p>
-    </div>)
+      <p className={styles.title}>{project.title}</p>
+
+      <div className="items">
+        {project.intro.split(" ").map((item) => {
+          return (
+            <p key={crypto.randomUUID()} className={styles.intro}>
+              {item}
+            </p>
+          );
+        })}
+      </div>
+
+      <div className={styles.techAndDate}>
+        <p className={styles.tech}>{project.tech.join(" / ")}</p>
+        <p className={styles.date}> 24 June</p>
+      </div>
+    </div>
+  );
+}
+
+{
+  /* <div className="items">
+        {project.intro.split(" ").map((item) => {
+          return (
+            <p key={crypto.randomUUID()} className={styles.intro}>
+              {item}
+            </p>
+          );
+        })}
+      </div> */
 }
--- src/types/Project.tsx
+++ src/types/Project.tsx
@@ -1,6 +1,8 @@
 export type Project = {
-  id: number,
-  author: string,
-  title: string,
-  create_date: Date,
-}
+  id: number;
+  author: string;
+  title: string;
+  create_date: Date;
+  intro: string;
+  tech: string[];
+};
