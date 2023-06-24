2c2bd896b5adca77d4912847382b26129f105890
n4mr3g <german.n.piccioni@gmail.com>
2023-06-24 11:16:23 +0200

    build(client): add clerk and clerk/themes packages
    Include also a ClerkProvider element on _app.tsx
    
    issue #5

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      {...pageProps}
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 }
