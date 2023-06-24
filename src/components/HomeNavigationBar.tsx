852a8cad019be2328a292ac4c331289724c43b94
Weronika Materkowska <w.m.materkowska@gmail.com>
2023-06-24 16:05:58 +0200

    feat(logout menu): add clerk driven profile and logout menu

--- src/components/HomeNavigationBar.tsx
+++ src/components/HomeNavigationBar.tsx
@@ -1,15 +1,29 @@
 import styles from "@/styles/HomeNavigationBar.module.css";
-import { SignInButton } from "@clerk/nextjs";
+import { SignInButton, UserButton, useAuth, useUser } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function HomeNavigationBar() {
+
+  const { userId } = useAuth();
+  const { user } = useUser();
+
+
   return (
     <>
       <div className={styles.container}>
         <div className={styles.logo}>SetApp</div>
         <div className={styles.navOptions}>
-          <SignInButton mode="modal">
-            <button className={styles.loginBtn}>Login</button>
-          </SignInButton>
+          {!userId ? (
+            <SignInButton mode="modal">
+              <button className={styles.loginBtn}>Login</button>
+            </SignInButton>
+          ) : (
+            <Link href="/profile" className="user-name">
+              <UserButton afterSignOutUrl="/" />
+              <span>{user?.username}</span>
+            </Link>
+          )
+          }
         </div>
       </div>
     </>
