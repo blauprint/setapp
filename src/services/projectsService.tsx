f7c778a79a3cba5f07667853066aeb6a1a5e3bec
tawfeeqm <tawfeeqmeeri@gmail.com>
2023-06-26 11:47:15 +0200

    added auth property to the request header

--- src/services/projectsService.tsx
+++ src/services/projectsService.tsx
@@ -1,10 +1,17 @@
 import { Auth } from '@/types/Auth';
 import { Project } from '@/types/Project';
 
-const baseUrl = "http://localhost:5000"
+const baseUrl = "http://localhost:5000";
 
 export async function getProjects(auth: Auth): Promise<Project[]> {
-  const projectsPromise = await fetch(`${baseUrl}/projects`);
+  const options = {
+    method: 'GET', 
+    headers: {
+      'Content-Type': 'application/json',
+      'auth': JSON.stringify(auth),
+    },
+  }
+  const projectsPromise = await fetch(`${baseUrl}/projects`, options);
   const projects: Project[] = await projectsPromise.json();
 
   return projects;
