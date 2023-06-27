446573d17de7259921bbc124f608ef93cdee59ca
n4mr3g <german.n.piccioni@gmail.com>
2023-06-27 09:37:55 +0200

    fix(frontend): fix last commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
-import Head from "next/head";
-
 import IdeaInputForm from "@/components/IdeaInputForm";
-
-
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import Link from 'next/link'
 
 
 export default function Home() {
