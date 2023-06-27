c34d49416ad1eda7cceb5acd082db1ad6a379a3e
n4mr3g <german.n.piccioni@gmail.com>
2023-06-27 09:28:43 +0200

    refactor(typescript): rename type files, add tsconfig settings for paths

---
+++ src/types/Project.ts
@@ -0,0 +1,8 @@
+export type Project = {
+  id: number;
+  author: string;
+  title: string;
+  created_at: Date;
+  intro: string;
+  techStacks: string[];
+};
--- src/types/Project.tsx
+++
@@ -1,8 +0,0 @@
-export type Project = {
-  id: number;
-  author: string;
-  title: string;
-  created_at: Date;
-  intro: string;
-  techStacks: string[];
-};
