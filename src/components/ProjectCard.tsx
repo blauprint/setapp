e4c5a3f807401659084490b6cf594a7d563c50e1
Weronika Materkowska <w.m.materkowska@gmail.com>
2023-06-28 11:33:26 +0200

    feat(frontend): add data flow from project card to project details

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,15 +1,32 @@
-import { Project } from "@/types/Project";
 import styles from "@/styles/ProjectCard.module.css";
+import { ProjectData } from "@/types/typedefs";
+import { useRouter } from "next/router";
+import { useUser } from "@clerk/nextjs";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { addCurrentProject } from "@/redux/currentProjectSlice";
+import { store } from "@/redux/store";
 
-export default function ProjectCard({ project }: { project: Project }) {
+export default function ProjectCard({ project }: { project: ProjectData }) {
+
+  let router = useRouter();
+  const { user } = useUser()
+  let dispatch = useAppDispatch();
+
+
+  function handleClickOnProjectCard(project: ProjectData) {
+    const url = `/${user?.username ? user.username : user?.firstName}/${project.projectName}/output`
+    dispatch(addCurrentProject(project));
+    console.log(store.getState().currentProject, 'current project')
+    router.push(url)
+  }
+
   return (
-    <div className={styles.projectCard}>
-      <p className={styles.title}>{project.name}</p>
+    <div className={styles.projectCard} onClick={() => handleClickOnProjectCard(project)}>
+      <p className={styles.title}>{project.projectName}</p>
 
       <div className={styles.details}>
-        <p className={styles.tech}>{project.techStacks?.join(" / ")}</p>
-        {/* TODO the data is not dyamic at the moment  must intergrate date-fns for this*/}
-        <p className={styles.created_at}>24 June</p>
+        <p className={styles.tech}>{`${project.backend.framework.name}/${project.frontend.framework.name}`}</p>
+        <p className={styles.created_at}>{(new Date(project.createdAt)).toLocaleString()}</p>
       </div>
     </div>
   );
---
+++ src/redux/projectsSlice.ts
@@ -0,0 +1,23 @@
+import { AnyAction, PayloadAction, Slice, createSlice } from "@reduxjs/toolkit";
+import { RootState } from "./store";
+
+import { ProjectData } from "@/types/typedefs";
+
+const initialState: ProjectData[] = [];
+
+export const projectsSlice: Slice = createSlice({
+  name: 'projects',
+  initialState,
+  reducers: {
+    // add: (state: ProjectData[], action: PayloadAction<ProjectData[]>) => {
+    addProjects: (state: ProjectData[], action: AnyAction) => {
+      // state = [...state, action.payload];
+      // return state;
+      state.push(...action.payload);
+    },
+  },
+});
+
+export const { addProjects } = projectsSlice.actions;
+export const selectProject = (state: RootState) => state.projects;
+export default projectsSlice.reducer;
