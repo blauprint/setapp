e8a49f0a0459484eaa8734784a1b870a642d6116
n4mr3g <german.n.piccioni@gmail.com>
2023-07-02 16:36:18 +0200

    refactor(openai_api.tsx): remove unnecessary comments and code

--- src/pages/api/chat/openai_api.tsx
+++ src/pages/api/chat/openai_api.tsx
@@ -1,7 +1,8 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
 import { Configuration, OpenAIApi } from "openai-edge";
 import { OpenAIStream, StreamingTextResponse } from "ai";
+
+// IMPORTANT! Set the runtime to edge
+export const runtime = "edge";
 
 // Create an OpenAI API client (that's edge friendly!)
 const config = new Configuration({
@@ -10,28 +11,20 @@ const config = new Configuration({
 
 const openai = new OpenAIApi(config);
 
-// IMPORTANT! Set the runtime to edge
-export const runtime = "edge";
-
 export default async function POST(req: Request) {
-  // Extract the user's prompt from the body of the request
+
+  // Extract the messages from the body of the request
   const { messages } = await req.json();
-  // const {messages} = await req.json();
-  // const { prompt } = req.body;
-  console.log("MESSAGES: ", messages);
-  // console.log("REQ BODY", await JSON.parse(req.body));
 
   // Build the full prompt to be sent to OpenAI
   const fullPrompt = process.env.COMPLETION_PROMPT + `"` + messages[0].content + `",\n`;
-  // const fullPrompt = process.env.COMPLETION_PROMPT + `"` + prompt + `",\n`;
-  // console.log(fullPrompt);
 
   // Ask OpenAI for a streaming completion given the prompt
   const response = await openai.createChatCompletion({
     model: "gpt-3.5-turbo-16k",
     stream: true,
     max_tokens: 1200,
-    temperature: 0.2,
+    temperature: 0.5,
     messages: [
       {
         content: fullPrompt,
@@ -39,14 +32,6 @@ export default async function POST(req: Request) {
       },
     ],
   });
-
-  // const response = await openai.createCompletion({
-  //   model: 'text-davinci-003',
-  //   stream: true,
-  //   max_tokens: 1200,
-  //   temperature: 0.2,
-  //   prompt: fullPrompt,
-  // });
 
   // Convert the response into a friendly text-stream
   const stream = OpenAIStream(response);
