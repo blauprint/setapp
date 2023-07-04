b7b01d5a77be1a20012587b7d47002e0a11855c2
Weronika Materkowska <w.m.materkowska@gmail.com>
2023-07-04 19:57:16 +0200

    fix(todos): fix double rendering of todos, delete todosSlice

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,14 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import projectReducer from "@/redux/projectsSlice";
 import currentProjectReducer from "@/redux/currentProjectSlice"
 import selectedReducer from "@/redux/selectedSlice"
-import todoSlice from "./todoSlice";
 
 export const store = configureStore({
   reducer: {
     projects: projectReducer,
     currentProject: currentProjectReducer,
     selected: selectedReducer,
-    todos: todoSlice 
   },
 });
 
