3191c98fbe9e282bbed08f269b52bba94ebbd3dd
n4mr3g <german.n.piccioni@gmail.com>
2023-07-05 15:58:41 +0200

    fix(idea page): enforce auth for idea input page

--- src/pages/idea.tsx
+++ src/pages/idea.tsx
@@ -1,9 +1,15 @@
-import IdeaInputForm from "@/components/IdeaInputForm";
+import IdeaInputForm from '@/components/IdeaInputForm';
+import { RedirectToSignIn, SignedIn, SignedOut } from '@clerk/nextjs';
 
 export default function IdeaPage() {
   return (
     <>
-      <IdeaInputForm />
+      <SignedIn>
+        <IdeaInputForm />
+      </SignedIn>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
     </>
   );
 }
